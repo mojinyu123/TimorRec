63812db5da3f64f2dd4e1d8652caae441991dd47
ds_mjy <374151683@qq.com>
2022-05-19 21:47:40 +0800

    write utils by timor

---
+++ utils/args.py
@@ -0,0 +1,45 @@
+import argparse
+import string
+
+
+def data_preparation_args():
+    parser = argparse.ArgumentParser(description="data preparation")
+    parser.add_argument('--train_path', type=str, default='./train_data.csv')
+    parser.add_argument('--test_path', type=str, default='./test_data.csv')         
+    parser.add_argument('--train_data_path', type=str, default='./')
+    parser.add_argument('--test_data_path', type=str, default='./')
+    return parser.parse_args()
+
+def share_bottom_layer_args():
+    # feature_size, task_num, bottom_size, tower_size
+    parser = argparse.ArgumentParser(description="ShareBottomLayer") 
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--feature_size', type=int, default=499)
+    parser.add_argument('--task_num', type=int, default=2)
+    parser.add_argument('--bottom_size', type=int, default=117)
+    parser.add_argument('--tower_size', type=int, default=8)
+
+    parser.add_argument('--epoch', type=int, default=100)
+    parser.add_argument('--bs', type=int, default=100)
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--dataset', type=str, default="census")
+
+    return parser.parse_args()
+
+def mmoe_layer_args():
+    # feature_size, task_num, bottom_size, tower_size
+    parser = argparse.ArgumentParser(description="ShareBottomLayer") 
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--feature_size', type=int, default=499)
+    parser.add_argument('--gate_num', type=int, default=2)
+    parser.add_argument('--expert_num', type=int, default=8)
+    parser.add_argument('--expert_size', type=int, default=16)
+    parser.add_argument('--tower_size', type=int, default=8)
+
+    parser.add_argument('--epoch', type=int, default=100)
+    parser.add_argument('--bs', type=int, default=100)
+
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--dataset', type=str, default="census")
+
+    return parser.parse_args()
---
+++ utils/metric.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch.nn as nn
+from sklearn import metrics
+
+softmax = nn.Softmax(dim = 1)
+
+class auc_class():
+    def __init__(self):
+        self.pred_list = []
+        self.label_list = []
+        self.metric = metrics.roc_auc_score
+    
+    def update(self, pred, label):
+        pred = softmax(pred)
+        pred = pred[:, 1].cpu().numpy()
+        self.pred_list.extend(pred)
+        self.label_list.extend(label.cpu().numpy())
+
+    def cal(self):
+        return round(self.metric(self.label_list, self.pred_list), 4)
+    
+    def refresh(self):
+        self.pred_list = []
+        self.label_list = []
